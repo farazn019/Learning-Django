885a0cc639cd771cc87dafc141bc482989bbf1a0
FarazNaseem <faraz.n019@outlook.com>
2020-08-03 14:33:24 -0400

    Created an app for the music section of the website, and now I have synced the music section with the database aswell.

--- music/views.py
+++ music/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("<script>alert('Hello and welcome to the music page')</script>")
