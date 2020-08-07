1418e335160c3855c8a49e26aa57afbc2fe679cb
FarazNaseem <faraz.n019@outlook.com>
2020-08-07 04:40:12 -0400

    Now, the name of the song in each Album will be shown, along with the information, whenever the user click on the name of a song title.

--- music/views.py
+++ music/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
+from .models import Album
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("<script>console.log('Hello and welcome to the music page')</script>")
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+
+    return HttpResponse(html)
 
 def details(request, album_id):
     return HttpResponse("<h3>The list:</h3>")
