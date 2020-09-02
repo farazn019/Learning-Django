91dc3889ab3b9e284f1dd12f0ccf5d30fe420c11
FarazNaseem <faraz.n019@outlook.com>
2020-09-01 23:04:01 -0400

    Added the templates to the flask blog.

--- Learning-Flask/flaskblog.py
+++ Learning-Flask/flaskblog.py
@@ -1,16 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'author': 'Faraz Naseem',
+        'title': 'Blog Post 1',
+        'content': 'The content for the first post',
+        'date_posted': 'August 31, 2020'
+    },
+    {
+        'author': 'Zaraf Maseen',
+        'title': 'Blog Post 2',
+        'content': 'The content for the second post',
+        'date_posted': 'September 1, 2020'
+    }
+]
+
+
 @app.route('/hello')
 def hello_world():
-    return "Hello World!"
-
+    return render_template('home.html', posts=posts)
 
 @app.route('/')
+@app.route('/main')
 def main_page():
-    return '<h2 style="text-align: center">This is THE main page</h2>'
+    return render_template('main.html')
 
 
 if __name__ == '__main__':
