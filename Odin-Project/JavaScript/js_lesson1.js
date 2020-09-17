7dae262eef50981ff159d554c9679fd2666d21f8
FarazNaseem <faraz.n019@outlook.com>
2020-09-17 18:13:23 -0400

    This is me learning JS

---
+++ Odin-Project/JavaScript/js_lesson1.js
@@ -0,0 +1,16 @@
+
+//This function will be called inside of the js_lesson1.html file
+function operations(){
+	console.log(23 + 97);
+	console.log((4 + 6 + 9)/77);
+	
+	let a = 10;
+	console.log(a);
+	console.log(9 * a);
+	let b = 7*a;
+	console.log(b);
+	const max = 57;
+	percentage = actual/max;
+	console.log(percentage);
+}
+
